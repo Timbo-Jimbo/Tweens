b929f1c43405142e1cbe69092212dcdae19428f9
Tim Aksu <timbo@timbojimbo.com>
2026-06-05 20:30:45 +1000

    initial scaffold

---
+++ README.md
@@ -0,0 +1,6 @@
+# Timbo Jimbo - Tweens
+
+More info to come..!
+
+> [!WARNING]
+> This package is new - use at your own risk! :)
---
+++ com.timbojimbo.tweens/Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TimboJimboEditor.Tweens")]
+[assembly: InternalsVisibleTo("TimboJimboTests.Tweens")]
